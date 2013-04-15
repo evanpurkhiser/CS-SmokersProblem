1db6a438fe45f0fac86e3b6aedcd0e09be3234d6
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-04-15 00:54:56 -0400

    Rework the program for multiple agents

--- smoke.c
+++ smoke.c
@@ -6,109 +6,108 @@
 #include <errno.h>
 #include <unistd.h>
 
-sem_t smoker_waits[3];
+bool papers_on_table  = false
+bool tobacco_on_table = false
+bool matches_on_table = false;
+
+// Setup our smoker threads
+pthread_t smoker_threads[6];
+
+// Each smoker semaphore represents when a smoker has the items they need
+sem_t smoker_semaphors[3];
+
+// This is an array of strings describing what each smoker type needs
+char* smoker_needs[3] = { "matches & tobacco", "matches & paper", "tobacco & paper" };
+
+// An agent semaphore represents items on the table
 sem_t agent;
 
-typedef void *(*smokers_t)(void *);
-
 /**
- * Smoker 0 has an infinite supply of paper
+ * Smoker function, handles waiting for the item's that they need, and then
+ * smoking. Repeat this three times
  */
-void* smoker_0(void* arg)
+void* smoker(void* arg)
 {
-	while (true)
+	int smoker_id = *(int*) arg;
+	int type_id   = smoker_id % 3;
+
+	// Smoke 3 times
+	for (int i = 0; i < 3; ++i)
 	{
-		nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
-		printf("\033[0;33mSmoker zero \033[0;31m>>\033[0m Waiting to smoke (has paper)\n");
+		printf("\033[0;37mSmoker %d \033[0;31m>>\033[0m Waiting for %s\n",
+			smoker_id, smoker_needs[type_id]);
 
-		sem_wait(&smoker_waits[0]);
-		printf("\033[0;33mSmoker zero \033[0;32m<<\033[0m Received matches and tobacco. Now making cigarette.\n");
+		// Wait for the proper combination of items to be on the table
+		sem_wait(&smoker_semaphors[type_id]);
 		sem_post(&agent);
-		printf("\033[0;33mSmoker zero \033[0;37m--\033[0m Smoking cigarette.\n");
 
+		// We're smoking now
+		printf("\033[0;37mSmoker %d \0332[0;32m<<\033[0m Now smoking", smoker_id);
 	}
 
 	return NULL;
 }
 
-/**
- * Smoker 1 has an infinite supply of matches
- */
-void* smoker_1(void* arg)
-{
-	while (true)
-	{
-		nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
-		printf("\033[0;34mSmoker one  \033[0;31m>>\033[0m Waiting to smoke (has matches)\n");
-
-		sem_wait(&smoker_waits[1]);
-		printf("\033[0;34mSmoker one  \033[0;32m<<\033[0m Received paper and tobacco. Now making cigarette.\n");
-		sem_post(&agent);
-		printf("\033[0;34mSmoker one  \033[0;37m--\033[0m Smoking cigarette.\n");
-	}
-
-	return NULL;
-}
+// The mutex protectes the pusher from losing
+sem_t mutex;
 
 /**
- * Smoker 2 has an infinite supply of tobacco
+ * The pusher is responsible for releasing the proper smoker semaphore when the
+ * right item's are on the table.
  */
-void* smoker_2(void* arg)
+void* pusher(void* arg)
 {
-	while (true)
-	{
-		nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
-		printf("\033[0;35mSmoker two  \033[0;31m>>\033[0m Waiting to smoke (has tobacco)\n");
 
-		sem_wait(&smoker_waits[2]);
-		printf("\033[0;35mSmoker two  \033[0;32m<<\033[0m Received matches and paper. Now making cigarette.\n");
-		sem_post(&agent);
-		printf("\033[0;35mSmoker two  \033[0;37m--\033[0m Smoking cigarette.\n");
-	}
-
-	return NULL;
 }
+
 
 /**
  * The main thread handles the agent's arbitration of items.
  */
 int main(int argc, char* arvg[])
 {
-	// Seed our random nmber since we will be using random numbers
+	// Seed our random number since we will be using random numbers
 	srand(time(NULL));
 
-	// Initalize our agent semaphore
+	// There is only one agent semaphore since only one set of items may be on
+	// the table at any given time. A values of 1 = nothing on the table
 	sem_init(&agent, 0, 1);
 
-	// Setup our smoker threads and function refs
-	pthread_t smoker_threads[3];
-	smokers_t smoker_functions[3] = { smoker_0, smoker_1, smoker_2 };
+	// Initalize te mutext semaphore
+	sem_init(&mutex, 0, 1);
 
-	// Create the smoker threads and initalize the semaphores
+	// Initialize the semaphores for each of the 2 different types of smokers
 	for (int i = 0; i < 3; ++i)
 	{
-		sem_init(&smoker_waits[i], 0, 0);
+		sem_init(&smoker_semaphors[i], 0, 0);
+	}
 
-		if(pthread_create(&smoker_threads[i], NULL, smoker_functions[i], NULL) == EAGAIN)
+	// Smoker ID's will be passed to the threads. Allocate the ID's on the stack
+	int smoker_ids[6];
+
+	// Create the 6 smoker threads with IDs
+	for (unsigned int i = 0; i < 6; ++i)
+	{
+		smoker_ids[i] = i;
+
+		if(pthread_create(&smoker_threads[i], NULL, smoker, &smoker_ids[i]) == EAGAIN)
 		{
-			perror("Insufficent resources to create thread\n");
+			perror("Insufficient resources to create thread");
 			return 0;
 		}
 	}
 
-	// Let the agent begin distributing his items
-	while (true)
-	{
-		// Lock the agent once he's
-		sem_wait(&agent);
 
-		// Distribute two items for one of the three the smokers
-		sem_post(&smoker_waits[rand() % 3]);
-	}
 
-	// Join all of the smoker threads on exit
-	for (int i = 0; i < 3; ++i)
+
+
+
+
+	// Join all threads at the end
+	for (int i = 0; i < 6; ++i)
 	{
 		pthread_join(smoker_threads[i], NULL);
 	}
+
+	return 0;
 }
