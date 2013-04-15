c77453d87c19c41e6b80c766722f40d94f77d7d1
Evan Purkhiser <evanpurkhiser@gmail.com>
2013-04-14 21:24:29 -0400

    Setup smoker loops with semaphor waits

--- smoke.c
+++ smoke.c
@@ -2,11 +2,12 @@
 #include <semaphore.h>
 #include <stdbool.h>
 #include <stdio.h>
+#include <stdlib.h>
 #include <errno.h>
+#include <unistd.h>
 
-bool paper_on_table  = false;
-bool match_on_table  = false;
-bool tabaco_on_table = false;
+sem_t smoker_waits[3];
+sem_t agent;
 
 typedef void *(*smokers_t)(void *);
 
@@ -15,7 +16,14 @@ typedef void *(*smokers_t)(void *);
  */
 void* smoker_0(void* arg)
 {
-	printf("Smoker zero waiting to smoke (has paper)\n");
+	while (true)
+	{
+		nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
+		printf("Smoker zero waiting to smoke (has paper)\n");
+
+		sem_wait(&smoker_waits[0]);
+		printf("Smoker zero recieved matchges and tabaco. Now smoking.\n");
+	}
 
 	return NULL;
 }
@@ -25,7 +33,14 @@ void* smoker_0(void* arg)
  */
 void* smoker_1(void* arg)
 {
-	printf("Smoker one waiting to smoke (has matches)\n");
+	while (true)
+	{
+		nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
+		printf("Smoker one waiting to smoke (has matches)\n");
+
+		sem_wait(&smoker_waits[1]);
+		printf("Smoker zero recieved matchges and tabaco. Now smoking.\n");
+	}
 
 	return NULL;
 }
@@ -35,22 +50,38 @@ void* smoker_1(void* arg)
  */
 void* smoker_2(void* arg)
 {
-	printf("Smoker two waiting to smoke (has tabaco)\n");
+	while (true)
+	{
+		nanosleep((struct timespec[]){{0, rand() % 200000000}}, NULL);
+		printf("Smoker two waiting to smoke (has tabaco)\n");
+
+		sem_wait(&smoker_waits[2]);
+		printf("Smoker zero recieved matchges and tabaco. Now smoking.\n");
+	}
 
 	return NULL;
 }
 
 /**
- * The main thread handles the agent's abritatin
+ * The main thread handles the agent's abritatin of items.
  */
 int main(int argc, char* arvg[])
 {
+	// Seed our random nmber since we will be using random numbers
+	srand(time(NULL));
+
+	// Initalize our agent semaphore
+	sem_init(&agent, 0, 0);
+
+	// Setup our smoker threads and function refs
 	pthread_t smoker_threads[3];
 	smokers_t smoker_functions[3] = { smoker_0, smoker_1, smoker_2 };
 
-	// Create the smoker threads
+	// Create the smoker threads and initalize the semaphores
 	for (int i = 0; i < 3; ++i)
 	{
+		sem_init(&smoker_waits[i], 0, 0);
+
 		if(pthread_create(&smoker_threads[i], NULL, smoker_functions[i], NULL) == EAGAIN)
 		{
 			perror("Insufficent resources to create thread\n");
